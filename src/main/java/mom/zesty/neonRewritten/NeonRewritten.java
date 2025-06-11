6524d7d898929cbd685db9ffad72958e15cb517d
NotAzex <azexdid@gmail.com>
2025-06-11 23:46:18 +0300

    Push files

---
+++ src/main/java/mom/zesty/neonRewritten/NeonRewritten.java
@@ -0,0 +1,19 @@
+package mom.zesty.neonRewritten;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NeonRewritten extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Loading Neon Rewritten...");
+        getLogger().info(" | Running on Java " + System.getProperty("java.version"));
+        getLogger().info(" | Server version " + Bukkit.getVersion());
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
